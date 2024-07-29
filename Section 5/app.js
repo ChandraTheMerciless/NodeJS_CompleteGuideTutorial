e5e3bcbafbd2ce1d9234749b3c3ddc6296ee27ef
chandrathemerciless <chandra.asar@gmail.com>
2024-07-29 19:55:21 -0400

    Making progress in express ection of tutorial

--- Section 5/app.js
+++ Section 5/app.js
@@ -1,9 +1,29 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+// registers a middleware function that will parse the body of the request. Automatically calls next() to move to next middleware
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    res.send(
+        `<form action="/product" method="POST">
+            <input type="text" name="title" />
+            <button type="submit">Add Product</button>    
+        </form>`
+    );
+    // do not call next() here to prevent '/' from also running if /add-product runs 
+});
 
-server.listen(3000);
+// app.post is like app.use but only fires for post requests. Works for other HTTP request types
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express</h1>'); // allows us to attach a body of type any. from express js so you don't have to have a res.setHeader()
+});
+
+app.listen(3000);
