a71b16da7de2aa6011a6956941d07cdfff0b5110
chandrathemerciless <chandra.asar@gmail.com>
2024-08-01 12:47:47 -0400

    Added html pages to load templates

--- Section 5/routes/admin.js
+++ Section 5/routes/admin.js
@@ -1,19 +1,17 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
+
+// bioth can have the same route since one is get and one is post
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        `<form action="/product" method="POST">
-            <input type="text" name="title" />
-            <button type="submit">Add Product</button>    
-        </form>`
-    );
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
     // do not call next() here to prevent '/' from also running if /add-product runs 
 });
 
 // app.post is like app.use but only fires for post requests. Works for other HTTP request types
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
--- Section 5/routes/shop.js
+++ Section 5/routes/shop.js
@@ -1,10 +1,12 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 // get, post, etc use exact routes. use does not!
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from express</h1>'); // allows us to attach a body of type any. from express js so you don't have to have a res.setHeader()
+    // paths to html templates need to be absolute paths, so we need to use the path.join utility to create that
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
